f36363a3197c1118f70017c33d8e0a66c098c599
Iman <imanali.cse@gmail.com>
2021-03-06 15:18:55 +0600

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AminDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,16 +32,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 Route::prefix('admin')->namespace('Admin')->name('admin.')->middleware('admin')->group(static function() {
-    //Route::middleware('admin')->group(static function () {
-        //Route::get('/', [DashboardController::class, 'index']);
-        Route::get('/user', function (){
-            echo "<pre>";
-            print_r(route('admin.user'));
-            echo "</pre>";
-
-            return "Hello admin";
-        })->name('user');
-    //});
+    Route::get('/', [AminDashboardController::class, 'index']);
+    Route::get('/user', function (){
+        echo "<pre>";
+        print_r(route('admin.user'));
+        echo "</pre>";
+        return "Hello admin user";
+    })->name('user');
 });
 
 //Subdomain Routing
